0446804ac274445635b2e42a99f33a9015614ea4
Alexey Debelov <alienjustmail@gmail.com>
2018-08-27 14:34:30 +0500

    Finally assemblies loading working well

--- Source/GatewayApp/CompositionRoot.cs
+++ Source/GatewayApp/CompositionRoot.cs
@@ -13,6 +13,8 @@ using AJ.Std.Reflection;
 using AJ.Std.Text;
 using AJ.Std.Text.Contracts;
 using Audience;
+using McMaster.NETCore.Plugins;
+using Microsoft.Extensions.DependencyModel;
 
 namespace GatewayApp {
   class CompositionRoot : ICompositionRoot {
@@ -26,41 +28,53 @@ namespace GatewayApp {
           new ThreadFormatter(" > ", false, true, false), new DateTimeFormatter(" > ")
         })));
 
-    [ImportMany] private IEnumerable<ICompositionPart> _compositionParts { get; set; }
+    //[ImportMany] public IEnumerable<ICompositionPart> _compositionParts { get; set; }
+    private readonly List<ICompositionPart> _compositionParts;
+    
 
     public CompositionRoot() {
       Log.Log("CompositionRoot constructor called");
+      _compositionParts = new List<ICompositionPart>();
       var dllFiles = Directory.GetFiles(typeof(CompositionRoot).GetAssemblyDirectoryPath(), "*.dll").ToList();
-      var assemblies = dllFiles.Select(p => {
-        Console.WriteLine(p);
-        return AssemblyLoadContext.Default.LoadFromAssemblyPath(p);
-      }).Where(x => x != null).ToList();
 
+      //var loaders = new List<PluginLoader>();
+      var assemblies = new List<Assembly>();
 
-/*var goodAssemblises = new List<Assembly>();
-
-Log.Log("Assemblies count = " + assemblies.Count);
-
-foreach (var assembly in assemblies) {
-  try {
-    Log.Log(assembly.FullName);
-    goodAssemblises.Add(assembly);
-  }
-  catch (Exception e) {
-    Console.WriteLine(e);
-    //continue;
-  }
-    }*/
-
-      //var configuration = new ContainerConfiguration().WithAssemblies(goodAssemblises);
-      //var configuration = new ContainerConfiguration().WithAssemblies(assemblies);
-      var configuration = new ContainerConfiguration().WithPart(typeof(ICompositionPart));
+      foreach (var dllFile in dllFiles) {
+        try {
+          assemblies.Add(AssemblyLoader.LoadFromAssemblyPath(dllFile));
+          //loaders.Add(PluginLoader.CreateFromAssemblyFile(dllFile, new[] {typeof(ICompositionPart)}));
+          Log.Log("Loaded assembly from " + dllFile);
+        }
+        catch (Exception e) {
+          Console.WriteLine(e);
+        }
+      }
+      
+      /*
+      var configuration = new ContainerConfiguration().WithAssemblies(assemblies);
       using (var container = configuration.CreateContainer()) {
         _compositionParts = container.GetExports<ICompositionPart>();
+      }*/
+      foreach (var assembly in assemblies) {
+        var types = assembly.GetTypes().Where(t => typeof(ICompositionPart).IsAssignableFrom(t) && !t.IsAbstract);
+        foreach (var t in types) {
+          var part = (ICompositionPart) Activator.CreateInstance(t);
+          _compositionParts.Add(part);
+        }
       }
+      /*
+      foreach (var loader in loaders) {
+        foreach (var pluginType in loader.LoadDefaultAssembly().GetTypes()
+          .Where(t => typeof(ICompositionPart).IsAssignableFrom(t) && !t.IsAbstract)) {
+          // This assumes the implementation of IPlugin has a parameterless constructor
+          ICompositionPart plugin = (ICompositionPart) Activator.CreateInstance(pluginType);
 
+          Console.WriteLine($"Created plugin instance '{plugin.Name}'.");
+          _compositionParts.Add(plugin);
+        }
+      }*/
 
-//_compositionParts = exportedTypes.ToList();
 
       try {
         foreach (var compositionPart in _compositionParts) {
@@ -85,6 +99,44 @@ foreach (var assembly in assemblies) {
 
     public ICompositionPart GetPartByName(string partName) {
       return _compositionParts.First(cp => cp.Name == partName);
+    }
+  }
+
+  public static class AssemblyLoader {
+    public static Assembly LoadFromAssemblyPath(string assemblyFullPath) {
+      var fileNameWithOutExtension = Path.GetFileNameWithoutExtension(assemblyFullPath);
+      var fileName = Path.GetFileName(assemblyFullPath);
+      var directory = Path.GetDirectoryName(assemblyFullPath);
+
+      var inCompileLibraries = DependencyContext.Default.CompileLibraries.Any(l =>
+        l.Name.Equals(fileNameWithOutExtension, StringComparison.OrdinalIgnoreCase));
+      var inRuntimeLibraries = DependencyContext.Default.RuntimeLibraries.Any(l =>
+        l.Name.Equals(fileNameWithOutExtension, StringComparison.OrdinalIgnoreCase));
+
+      var assembly = (inCompileLibraries || inRuntimeLibraries)
+        ? Assembly.Load(new AssemblyName(fileNameWithOutExtension))
+        : AssemblyLoadContext.Default.LoadFromAssemblyPath(assemblyFullPath);
+
+      if (assembly != null)
+        LoadReferencedAssemblies(assembly, fileName, directory);
+
+      return assembly;
+    }
+
+    private static void LoadReferencedAssemblies(Assembly assembly, string fileName, string directory) {
+      var filesInDirectory = Directory.GetFiles(directory).Where(x => x != fileName)
+        .Select(x => Path.GetFileNameWithoutExtension(x)).ToList();
+      var references = assembly.GetReferencedAssemblies();
+
+      foreach (var reference in references) {
+        if (filesInDirectory.Contains(reference.Name)) {
+          var loadFileName = reference.Name + ".dll";
+          var path = Path.Combine(directory, loadFileName);
+          var loadedAssembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(path);
+          if (loadedAssembly != null)
+            LoadReferencedAssemblies(loadedAssembly, loadFileName, directory);
+        }
+      }
     }
   }
 }
