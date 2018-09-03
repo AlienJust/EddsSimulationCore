c3fff2a8517cb179b87bff9cb5b485e8678f4540
Alexey Debelov <alienjustmail@gmail.com>
2018-09-03 12:10:21 +0500

    Working on commands mode for Lora

---
+++ Source/Controllers.Lora/InteleconAnyCommand.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using PollSystem.CommandManagement.Channels;
+
+namespace Controllers.Lora {
+	internal sealed class InteleconAnyCommand : IInteleconCommand {
+		public InteleconAnyCommand(object identifier, int code, IReadOnlyList<byte> data) {
+			Identifier = identifier;
+			Code = code;
+			Data = data;
+		}
+		public object Identifier { get; }
+		public int Code { get; }
+		public IReadOnlyList<byte> Data { get; }
+	}
+}
---
+++ Source/Controllers.Lora/LoraControllerFullInfo.cs
@@ -0,0 +1,17 @@
+﻿using Controllers.Gateway.Attached;
+
+namespace Controllers.Lora {
+	internal class LoraControllerFullInfo {
+		public LoraControllerInfoSimple LoraControllerInfo { get; }
+		public string RxTopicName { get; }
+		public string TxTopicName { get; }
+		public AttachedObjectConfig AttachedControllerConfig { get; }
+
+		public LoraControllerFullInfo(LoraControllerInfoSimple loraControllerInfo, string rxTopicName, string txTopicName, AttachedObjectConfig attachedControllerConfig) {
+			LoraControllerInfo = loraControllerInfo;
+			RxTopicName = rxTopicName;
+			TxTopicName = txTopicName;
+			AttachedControllerConfig = attachedControllerConfig;
+		}
+	}
+}
