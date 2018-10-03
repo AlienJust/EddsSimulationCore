aa193d551704b9174775e73fcaccdb0124f3f064
Alexey Debelov <alienjustmail@gmail.com>
2018-10-03 22:21:07 +0500

    Gateway: added empty answer for time sync command

--- Source/Controllers.Gateway/GateController.cs
+++ Source/Controllers.Gateway/GateController.cs
@@ -18,12 +18,16 @@ namespace Controllers.Gateway {
 		public void GetDataInCallback(int command, IReadOnlyList<byte> data, Action<Exception, IReadOnlyList<byte>> callback) {
 			try {
 				switch (command) {
-					case 1:
+					case 1: // operative data via 1
 						var result1 = new byte[1];
 						_random.NextBytes(result1);
 						callback(null, result1);
 						break;
-					case 6:
+					case 2: // time sync
+						var result2 = new byte[0];
+						callback(null, result2);
+						break;
+					case 6: // operative data via 6
 						var result6 = data.ToList();
 						// Special type of attached counter = 250 means that command sended to gateway controller itself (c) Danila
 						if (result6[1] == 250) {
