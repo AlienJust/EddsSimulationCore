9b14e3a0a223400eb09ca259d475d222694788ce
Alexey Debelov <aj001@mail.ru>
2018-12-17 01:25:57 +0500

    Added DataRate setter in TxInfo json item

--- Source/Controllers.Lora/JsonBrocaar/TxInfoDataRate.cs
+++ Source/Controllers.Lora/JsonBrocaar/TxInfoDataRate.cs
@@ -1,14 +1,14 @@
 ﻿using Newtonsoft.Json;
 
 namespace Controllers.Lora.JsonBrocaar {
-  internal class TxInfoDataRate {
-    [JsonProperty(PropertyName = "modulation")]
-    public string Modulation { get; set; }
+	internal class TxInfoDataRate {
+		[JsonProperty(PropertyName = "modulation")]
+		public string Modulation { get; set; }
 
-    [JsonProperty(PropertyName = "bandwidth")]
-    public int Bandwidth { get; set; }
+		[JsonProperty(PropertyName = "bandwidth")]
+		public int Bandwidth { get; set; }
 
-    [JsonProperty(PropertyName = "spreadFactor")]
-    public int SpreadFactor { get; set; }
-  }
+		[JsonProperty(PropertyName = "spreadFactor")]
+		public int SpreadFactor { get; set; }
+	}
 }
--- Source/Controllers.Lora/JsonBrocaar/TxInfoItem.cs
+++ Source/Controllers.Lora/JsonBrocaar/TxInfoItem.cs
@@ -1,16 +1,16 @@
 ﻿using Newtonsoft.Json;
 
 namespace Controllers.Lora.JsonBrocaar {
-  internal class TxInfoItem {
-    [JsonProperty(PropertyName = "frequency")]
-    public int Frequency { get; set; }
+	internal class TxInfoItem {
+		[JsonProperty(PropertyName = "frequency")]
+		public int Frequency { get; set; }
 
-    [JsonProperty(PropertyName = "dataRate")]
-    public TxInfoDataRate DataRate { get; }
+		[JsonProperty(PropertyName = "dataRate")]
+		public TxInfoDataRate DataRate { get; set; }
 
-    [JsonProperty(PropertyName = "adr")] public bool Adr { get; set; }
+		[JsonProperty(PropertyName = "adr")] public bool Adr { get; set; }
 
-    [JsonProperty(PropertyName = "codeRate")]
-    public string CodeRate { get; set; }
-  }
+		[JsonProperty(PropertyName = "codeRate")]
+		public string CodeRate { get; set; }
+	}
 }
