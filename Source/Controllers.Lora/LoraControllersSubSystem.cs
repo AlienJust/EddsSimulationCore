afa39aff910eb605213f5e4a1da5b8b005782b35
Alexey Debelov <aj001@mail.ru>
2018-12-20 11:33:16 +0500

    Added support for LORA SDV v2 controller (attached type 52) Also updated configuration

--- Source/Controllers.Lora/LoraControllersSubSystem.cs
+++ Source/Controllers.Lora/LoraControllersSubSystem.cs
@@ -109,8 +109,8 @@ namespace Controllers.Lora {
 					var number = data[2];
 					Log.Log("[OK] Command code and data length are correct, att_channel=" + channel + ", att_type=" + type + ", att_number=" + number);
 
-					if (type != 49 && type != 50 && type != 51) {
-						Log.Log("[ER] Att_type != 49 && Att_type != 50 && Att_type != 51, LORA controllers subsystem does not handling such (" + type + ") att_type, return");
+					if (type != 49 && type != 50 && type != 51 && type != 52) {
+						Log.Log("[ER] Att_type != 49 && Att_type != 50 && Att_type != 51 && type != 52, LORA controllers subsystem does not handling such (" + type + ") att_type, return");
 						return;
 					}
 
